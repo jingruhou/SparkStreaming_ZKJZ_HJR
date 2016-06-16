9f69d656f9aa5009adc8d86b8b76499b0ea2d806
houjingru <1627383642@qq.com>
2016-06-16 17:12:42 +0800

    2016-6 -16  对数据去重后的结果进行拆分---完成

--- src/zkjz/hjr/xjy/distinct.scala
+++ src/zkjz/hjr/xjy/distinct.scala
@@ -3,15 +3,16 @@ package zkjz.hjr.xjy
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
-  * Created by Administrator on 2016/6/14.
+  * Created by Administrator on 2016/6/16.
   */
-object test {
+object distinct {
   def main(args:Array[String]): Unit ={
     val conf = new SparkConf().setAppName("test").setMaster("local")
     val sc = new SparkContext(conf)
 
     // 0 加载数据
     val data = sc.textFile("D:/streamingData/sql/1.txt").cache()
+    data.foreach(println)
     // 1 第三列去重后的新RDD
     val column3RDD = data.map(line =>line.split("\t")(3)).map(line =>{
       var list = line.split(",").toList.distinct
@@ -24,13 +25,9 @@ object test {
     // 2 使用zip算子合并两个RDD结果
     val zipResult = data.map(line =>{
       line.split("\t")(1) +" "+ line.split("\t")(2)
-    }).zip(column3RDD).map(line => line._1 +" "+ line._2)
+    }).zip(column3RDD).map(line => line._1 +","+ line._2)
     zipResult.foreach(println)
-
-    /**
-      *  1 拆分结果
-      */
-
-
+    //9829135 2010-4-20 code1 code2 code3
+    //9829135 2010-6-26 code1 code2 code3
   }
 }
---
+++ src/zkjz/hjr/xjy/split.scala
@@ -0,0 +1,27 @@
+package zkjz.hjr.xjy
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by Administrator on 2016/6/16.
+  */
+object split {
+  def main(args:Array[String]): Unit ={
+    val conf = new SparkConf().setAppName("splitResult_zkjz").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile("D:/streamingData/sql/2.txt")
+    val codes = data.map(line => {
+      var newline = line.split("\t")
+      var firstline = newline(0)+"\t"+newline(1)+"\t"
+      var codeline = newline(2).split(" ")
+
+      var lastline = ""
+      for(i <- 0 to codeline.length-1){
+          lastline += firstline+codeline(i)+"\n"
+      }
+      lastline
+    })
+    codes.foreach(println)
+  }
+}

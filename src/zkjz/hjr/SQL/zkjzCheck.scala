438d174d87f7a3b7f78de2bee87d610adc593e96
houjingru <1627383642@qq.com>
2016-07-01 16:02:43 +0800

    2016-6-30 添加zkjz88注释（2）

--- src/zkjz/hjr/SQL/zkjzCheck.scala
+++ src/zkjz/hjr/SQL/zkjzCheck.scala
@@ -3,12 +3,12 @@ package zkjz.hjr.SQL
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
-  * Created by Administrator on 2016/6/25.
+  * Created by Administrator on 2016/7/1.
   */
-object zkjz88 {
+object zkjzCheck {
   def main(args:Array[String]): Unit ={
     //初始化配置
-    val conf = new SparkConf().setAppName("zkjz_hjr")
+    val conf = new SparkConf().setAppName("check_zkjz_hjr")
     val sc = new SparkContext(conf)
 
     //加载文件
@@ -30,38 +30,37 @@ object zkjz88 {
     }
     //处理门诊表的业务逻辑
     val outclinical = outclinical_diago_rdd.filter(_.split("\001").length == 4).map(line =>{
-      /**
-        * 处理门诊表的每一行数据
-        */
       var strs = line.split("\001")
-      var l = strs(3)//(门诊表业务逻辑---1)拿出第四个字段（DIAG_NAME）
+      var l = strs(3)
       var firstline = strs(0)+"\001"+strs(1)+"\001"+strs(2)+"\001"//（门诊表业务逻辑---2）拿出前三个字段
       var lastline = ""
       var s = line
-      var m = l.length//（门诊表业务逻辑--3）拿出第四个字段的长度
-      while (m >= 1) {//第四个字段---诊断名不能为空（DIAG_NAME的长度必须>=1）
-        var j = 0
+      var m = l.length
+      while (m >= 1) {
+      var j = 0
         while (j < l.length - m + 1) {
           var s3 = l.substring(j, j + m)
-          if (map.contains(s3)) {//去Map里面查找有没有第四个字段
+          if (map.contains(s3)) {
             //s=s.replace(s3,map(s3)+" ")
-            lastline += map(s3)+","//检查出Map里面包含了第四个字段（用词库里面的name1,name2,name3...）
-            l = l.replace(s3,"")//将第四个字段替换为空
+            lastline += map(s3)+","
+            l = l.replace(s3, "")
           }
           j = j + 1
-        }//一层循环---（先是1234对应找，然后234，对应找；再就是34对应找...）
+        }
         m = m - 1
-      }//二层循环---(先是123对应找，然后23，对应找；再就是3对应找...)
+      }
       firstline+lastline
+      //数据校验
     })
+    //结果校验
     /**
       * ReduceByKey过程
       */
     val pairs = outclinical.filter(_.split("\001").length == 4).map(line => {
 
-     // if(line.split("\001").length == 4) {
-        val col2 = line.split("\001")
-        (col2(1) + "\001" + col2(2), col2(3))
+      // if(line.split("\001").length == 4) {
+      val col2 = line.split("\001")
+      (col2(1) + "\001" + col2(2), col2(3))
       //}
     })
 
